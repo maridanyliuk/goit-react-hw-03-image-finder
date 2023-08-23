5f4b04c08f412f673db0c0a97f1fca19fdaf1a57
Maryna Danyliuk <maruna.danulyk@gmail.com>
2023-08-23 21:29:10 +0300

    hometask

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,74 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { SearchBar } from './SearchBar/SearchBar';
+import { fetchImages } from '../api';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { Button } from './Button/Button';
+import { Modal } from './Modal/Modal';
+import { Loader } from './Loader/Loader';
+
+export class App extends Component {
+  state = {
+    images: [],
+    query: '',
+    page: 1,
+    loading: false,
+    showModal: false,
+    largeImageURL: '',
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.query !== this.state.query) {
+      this.fetchImages();
+    }
+    if (prevState.images.length !== this.state.images.length) {
+      window.scrollTo({
+        top: document.documentElement.scrollHeight,
+        behavior: 'smooth',
+      });
+    }
+  }
+
+  onChangeQuery = query => {
+    this.setState({ query, page: 1, images: [] });
+  };
+
+  fetchImages = () => {
+    const { query, page } = this.state;
+    const params = { query, page };
+
+    this.setState({ loading: true });
+
+    fetchImages(params)
+      .then(images =>
+        this.setState(prevState => ({
+          images: [...prevState.images, ...images],
+          page: prevState.page + 1,
+        }))
+      )
+      .finally(() => this.setState({ loading: false }));
+  };
+
+  openModal = largeImageURL => {
+    this.setState({ showModal: true, largeImageURL });
+  };
+
+  closeModal = () => {
+    this.setState({ showModal: false, largeImageURL: '' });
+  };
+
+  render() {
+    const { images, loading, showModal, largeImageURL } = this.state;
+    const shouldRenderLoadMoreButton = images.length > 0 && !loading;
+
+    return (
+      <div className="App">
+        <SearchBar onSubmit={this.onChangeQuery} />
+        <ImageGallery images={images} onClick={this.openModal} />
+        {loading && <Loader />}
+
+        {shouldRenderLoadMoreButton && <Button onClick={this.fetchImages} />}
+        {showModal && <Modal onClose={this.closeModal} src={largeImageURL} />}
+      </div>
+    );
+  }
+}
